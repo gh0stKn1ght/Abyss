8d762c3045c8b6c595412908e2a7a188a07ae5a9
gh0stKn1ght <174645041+gh0stKn1ght@users.noreply.github.com>
2024-07-04 00:57:36 +0300

    Delete server/gen_private_key.py
    Deleted debug script

--- server/gen_private_key.py
+++
@@ -1,6 +0,0 @@
-from cryptography.hazmat.primitives.asymmetric import rsa
-from cryptography.hazmat.primitives import serialization
-
-private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
-pem = private_key.private_bytes(encoding=serialization.Encoding.PEM, format=serialization.PrivateFormat.PKCS8, encryption_algorithm=serialization.NoEncryption())
-open('private_key.txt', 'wb').write(pem)

6a4d7bd5db38967b0c15959109aa775238de2533
gh0stKn1ght <174645041+gh0stKn1ght@users.noreply.github.com>
2024-07-04 17:35:32 +0300

    Update reg_user.py

--- server/reg_user.py
+++ server/reg_user.py
@@ -1,5 +1,6 @@
 import hashlib
 import sqlite3
+import sys
 
 print('Add new user to database')
 login = input('Login: ')
@@ -7,7 +8,7 @@ password = input('Password: ')
 hash = hashlib.md5()
 hash.update(password.encode())
 password = hash.hexdigest()
-database = sqlite3.connect('users.db')
+database = sqlite3.connect(sys.argv[0].replace('reg_user.py', 'users.db'))
 cursor = database.cursor()
 cursor.execute('CREATE TABLE IF NOT EXISTS Credentials (id INTEGER PRIMARY KEY, login TEXT NOT NULL, password TEXT NOT NULL)')
 cursor.execute('INSERT INTO Credentials (login, password) VALUES (?, ?)', (login, password))
